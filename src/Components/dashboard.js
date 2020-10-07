dcf0d1648cff783f3d494f009948bfa8c12fffd5
Munnazzah Aslam <aslammunnazzah@gmail.com>
2020-10-07 12:40:55 +0500

    Update dashboard.js

--- src/Components/dashboard.js
+++ src/Components/dashboard.js
@@ -7,7 +7,8 @@ import Widgettext from './widgettext';
 import Widgetdoughnut from './widgetdoughnut';
 import {Col,Row,Container} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-dropdown/style.css';import 'react-dropdown/style.css';
+import 'react-dropdown/style.css';
+import 'react-dropdown/style.css';
 
 const config = {
   apiKey: 'AIzaSyDMu-Vw30ykPPmFT3cXeunzKEi4EahzglI',
